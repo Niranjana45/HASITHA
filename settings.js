1005497e079def04c2b47fc718064c10a5ac46f4
dark shadow_official <121376087+Niranjana45@users.noreply.github.com>
2025-03-09 16:29:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'මෙතනට ඔයාගෙ සිසන් id එක දාන්න' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7vA2wYiL#Gzn6oU5uDVlhLTC1GRvxupVTjU0ukjt63Z81fbYpuok' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
+SUDO: process.env.SUDO === undefined ? '94783223915' : process.env.SUDO,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
